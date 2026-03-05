6868f4a7e43e2169141dbd7aecf1aded3f021eef
grigorij018 <127332545+grigorij018@users.noreply.github.com>
2026-03-06 02:35:24 +0300

    Revert "Add bookings"

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,11 +2,12 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ShareItApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
 	}
+
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,12 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Data;
-
-@Data
+/**
+ * TODO Sprint add-controllers.
+ */
 public class Item {
-    private Long id;
-    private String name;
-    private String description;
-    private Boolean available;
-    private Long ownerId;
 }
